adc6550b04212216874ad698dd86f2efd74216c7
ernesto godoy <ernesto@ernestos-MacBook-Pro.local>
2020-04-01 11:05:01 -0700

    fixes foreign key create and show working

--- app/controllers/ages_controller.rb
+++ app/controllers/ages_controller.rb
@@ -1,2 +1,25 @@
 class AgesController < ApplicationController
+  def index
+    ages = Age.all
+    render json: ages
+  end
+        
+  def show
+    age = Age.find(params[:id])
+    render json: age.to_json
+  end
+        
+  def create
+    age = Age.create(age_params)
+        
+    if age.valid?
+      render json: age
+    else
+      render json: age.errors
+    end
+  end
+        
+  def age_params
+    params.require(:age).permit(:name_id, :age)
+  end
 end
--- db/migrate/20200331232355_create_ages.rb
+++ db/migrate/20200331232355_create_ages.rb
@@ -3,6 +3,7 @@ class CreateAges < ActiveRecord::Migration[6.0]
     create_table :ages do |t|
       t.integer :nameId
       t.integer :age
+      t.references :name, null: false, foreign_key: true
 
       t.timestamps
     end
